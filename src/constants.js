ec1119c485a8cf8fa131d4b96612f79abcc5aff6
JasonDollar <elrey33@gmail.com>
2019-08-06 16:02:26 +0200

    initial commit, fav characters chosen

---
+++ src/constants.js
@@ -0,0 +1,66 @@
+export const API_BASE_URL = 'https://futuramaapi.herokuapp.com/api/'
+
+export const CHARACTERS = [
+  {
+    id: 0,
+    name: 'Bender',
+    randQuote: `${API_BASE_URL}characters/Bender/1`,
+    quotesList: `${API_BASE_URL}characters/Bender/5`,
+  },
+  {
+    id: 1,
+    name: 'Fry',
+    randQuote: `${API_BASE_URL}characters/Fry/1`,
+    quotesList: `${API_BASE_URL}characters/Fry/5`,
+  },
+  {
+    id: 2,
+    name: 'Leela',
+    randQuote: `${API_BASE_URL}characters/Leela/1`,
+    quotesList: `${API_BASE_URL}characters/Leela/5`,
+  },
+  {
+    id: 3,
+    name: 'Dr Zoidberg',
+    randQuote: `${API_BASE_URL}characters/Dr-Zoidberg/1`,
+    quotesList: `${API_BASE_URL}characters/Dr-Zoidberg/5`,
+  },
+  {
+    id: 4,
+    name: 'Professor Farnsworth',
+    randQuote: `${API_BASE_URL}characters/Professor-Farnsworth/1`,
+    quotesList: `${API_BASE_URL}characters/Professor-Farnsworth/5`,
+  },
+  {
+    id: 5,
+    name: 'Amy',
+    randQuote: `${API_BASE_URL}characters/Amy/1`,
+    quotesList: `${API_BASE_URL}characters/Amy/5`,
+  },
+  {
+    id: 6,
+    name: 'Hermes',
+    randQuote: `${API_BASE_URL}characters/Hermes/1`,
+    quotesList: `${API_BASE_URL}characters/Hermes/5`,
+  },
+  {
+    id: 7,
+    name: 'Zapp Brannigan',
+    randQuote: `${API_BASE_URL}characters/Zapp-Brannigan/1`,
+    quotesList: `${API_BASE_URL}characters/Zapp-Brannigan/5`,
+  },
+  {
+    id: 8,
+    name: 'Kif',
+    randQuote: `${API_BASE_URL}characters/Kif/1`,
+    quotesList: `${API_BASE_URL}characters/Kif/5`,
+  },
+  {
+    id: 9,
+    name: 'Giant Bender',
+    randQuote: `${API_BASE_URL}characters/Giant-Bender/1`,
+    quotesList: `${API_BASE_URL}characters/Giant-Bender/5`,
+  },
+]
+
+// https://futuramaapi.herokuapp.com/api/characters/Bender/5
